9fb6798190bfcf6642ef1ae663fe78f24f977f00
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2024-05-28 13:07:21 +0300

    Поправил баги.

--- Chain/Program.cs
+++ Chain/Program.cs
@@ -1,5 +1,4 @@
-﻿// Program.cs
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -20,40 +19,122 @@ class Program
         int nextPort = int.Parse(args[2]);
         bool isInitiator = args.Length == 4 && args[3] == "true";
 
-        int X = int.Parse(Console.ReadLine());
+        int localX = int.Parse(Console.ReadLine());
 
-        using (UdpClient listener = new UdpClient(listeningPort))
-        using (UdpClient sender = new UdpClient())
+        Socket listener = new Socket(IPAddress.Any.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        listener.Bind(new IPEndPoint(IPAddress.Any, listeningPort));
+        Console.WriteLine("Listening port: {0}", listeningPort);
+
+        var senderIpAddress = Dns.GetHostAddresses(nextHost)[0];
+        Socket sender = new Socket(senderIpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint nextEndPoint = new IPEndPoint(senderIpAddress, nextPort);
+        Console.WriteLine("Next host: {0}", nextHost);
+        Console.WriteLine("Next port: {0}", nextPort);
+        Console.WriteLine("Is initiator: {0}", isInitiator);
+
+        if (isInitiator)
         {
-            IPEndPoint nextEndPoint = new IPEndPoint(IPAddress.Parse(nextHost), nextPort);
-
-            if (isInitiator)
-            {
-                await SendValue(sender, nextEndPoint, X);
-                X = await ReceiveValue(listener);
-                await SendValue(sender, nextEndPoint, X);
-                X = await ReceiveValue(listener);
-            }
-            else
-            {
-                X = Math.Max(X, await ReceiveValue(listener));
-                await SendValue(sender, nextEndPoint, X);
-                X = await ReceiveValue(listener);
-            }
-
-            Console.WriteLine(X);
+            SendMessage(sender, nextEndPoint, localX.ToString());
+            localX = int.Parse(ReceiveMessage(listener));
+            SendMessage(sender, nextEndPoint, localX.ToString());
+            localX = int.Parse(ReceiveMessage(listener));
+            Console.WriteLine("Answer: {0}", localX);
         }
+        else
+        {
+            int receivedY = int.Parse(ReceiveMessage(listener));
+            int maxValue = Math.Max(localX, receivedY);
+            SendMessage(sender, nextEndPoint, maxValue.ToString());
+            localX = int.Parse(ReceiveMessage(listener));
+            Console.WriteLine("Answer: {0}", localX);
+        }
+        
+        sender.Shutdown(SocketShutdown.Both);
+        listener.Shutdown(SocketShutdown.Both);
     }
 
-    static async Task<int> ReceiveValue(UdpClient client)
+    static void SendMessage(Socket socket, EndPoint endPoint, string message)
     {
-        var result = await client.ReceiveAsync();
-        return int.Parse(Encoding.UTF8.GetString(result.Buffer));
+        try
+        {
+            // CONNECT
+            socket.Connect(endPoint);
+
+            Console.WriteLine("Remote address of socket connection: {0}",
+                socket.RemoteEndPoint.ToString());
+
+            // Подготовка данных к отправке
+            byte[] msg = Encoding.UTF8.GetBytes(message + "<EOF>");
+
+            // SEND
+            int bytesSent = socket.Send(msg);
+
+            // RELEASE
+            socket.Close();
+        }
+        catch (ArgumentNullException ane)
+        {
+            Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+        }
+        catch (SocketException se)
+        {
+            Console.WriteLine("SocketException : {0}", se.ToString());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unexpected exception : {0}", e.ToString());
+        }
+
+        // byte[] data = Encoding.UTF8.GetBytes(message);
+        // await socket.SendToAsync(new ArraySegment<byte>(data), SocketFlags.None, endPoint);
     }
 
-    static async Task SendValue(UdpClient client, IPEndPoint endPoint, int value)
+    static string ReceiveMessage(Socket socket)
     {
-        byte[] data = Encoding.UTF8.GetBytes(value.ToString());
-        await client.SendAsync(data, data.Length, endPoint);
+        // byte[] buffer = new byte[1024];
+        // var result = await socket.ReceiveFromAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+        // return Encoding.UTF8.GetString(buffer, 0, result.ReceivedBytes);
+
+        try
+        {
+            // LISTEN
+            socket.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a client ot connect...");
+                // ACCEPT
+                Socket handler = socket.Accept();
+
+                Console.WriteLine("Receiving data...");
+                byte[] buf = new byte[1024];
+                string data = null;
+                while (true)
+                {
+                    // RECEIVE
+                    int bytesRec = handler.Receive(buf);
+
+                    data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                data = data.Substring(0, data.Length - 5);
+                Console.WriteLine("Received message: {0}", data);
+
+                // RELEASE
+                handler.Close();
+
+                return data;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        return "";
     }
 }
---
+++ Chain/start.ps1
@@ -0,0 +1,40 @@
+# Путь к проекту
+$project_path = "D:\Projects\ds-2024\Chain\Chain.csproj"
+
+# Выполнение сборки проекта
+Write-Host "Building the project..."
+dotnet build $project_path
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Build failed. Exiting script."
+    exit $LASTEXITCODE
+}
+
+# Массив значений для X
+$values = @(4, 17, -5, 32, 4, 5)
+
+# Количество процессов
+$num_processes = 6
+
+# Базовый порт для прослушивания
+$base_port = 1234
+
+# Хост для следующего соседа (для простоты используем localhost)
+$next_host = "localhost"
+
+# Путь к скомпилированному исполняемому файлу
+$executable_path = "D:\Projects\ds-2024\Chain\bin\Debug\net8.0\Chain.exe"
+
+# Запуск процессов
+for ($i = 0; $i -lt $num_processes; $i++) {
+    $listening_port = $base_port + $i
+    $next_port = $base_port + (($i + 1) % $num_processes)
+    $is_initiator = "true"
+
+    # Установить параметр инициатора для первого процесса
+    if ($i -gt 0) {
+        $is_initiator = "false"
+    }
+
+    # Запуск процесса в новом окне PowerShell
+    Start-Process powershell -ArgumentList "-NoExit", "-Command", "`$value = $($values[$i]); echo `$value; `$value | & '$executable_path' $listening_port $next_host $next_port $is_initiator"
+}
